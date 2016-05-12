c82632453ff7f4ca1905c85a1ae09f16223ecb7f
Chris Frank <chris@cfrank.org>
2016-05-13 01:53:28 +0300

    Remove and ignore 2nd stage compilation directory

--- assets/source/babel/main.js
+++
@@ -1,8 +0,0 @@
-/*
- * The entry point of the application
- * Calls the Xor class which actually does everything
-*/
-import Xor from './xor';
-document.addEventListener('DOMContentLoaded', () => {
-    new Xor();
-});
