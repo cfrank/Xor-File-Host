0df4da5c7841f42c8d2b31f7acec38d1f578a74c
Chris Frank <chris@cfrank.org>
2016-05-29 15:30:42 +0300

    Fix bug in get_id function

--- server/utils/get_id.php
+++ server/utils/get_id.php
@@ -3,11 +3,10 @@
      * Create a psuedo random string to use as the album id
      */
     function get_id(int $length): string{
-        $id_length = XOR_ALBUM_ID_LENGTH;
         $keyspace = XOR_KEYSPACE;
         $keyspace_length = strlen($keyspace);
         $id = '';
-        for($i = 0; $i < $id_length; ++$i){
+        for($i = 0; $i < $length; ++$i){
             $index_random = mt_rand(0, $keyspace_length - 1);
             $id .= $keyspace[$index_random];
         }
